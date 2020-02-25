2cf41fed9655c400fe8d2687b9b7eac3e1fdb988
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-02-25 22:44:49 +0100

    test gdk-pixbuf-pixdata too

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -14,4 +14,5 @@ class TestPackageConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             bin_path = os.path.join("bin", "test_package")
+            self.run("gdk-pixbuf-pixdata -v", run_environment=True)
             self.run(bin_path, run_environment=True)
